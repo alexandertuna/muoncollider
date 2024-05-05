b6f290787ced0d3dee4436e8f5279cfaaa5c5209
Alexander Tuna <alexander.tuna@gmail.com>
2024-05-05 12:48:13 -0500

    black it

--- detailedHadrons/main.py
+++ detailedHadrons/main.py
@@ -11,7 +11,8 @@ from typing import List
 import numpy as np
 import pandas as pd
 import matplotlib as mpl
-mpl.use('Agg')
+
+mpl.use("Agg")
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
@@ -26,14 +27,29 @@ ETAS = [
 encoding = "system:0:5,side:5:-2,module:7:8,stave:15:4,layer:19:9,submodule:28:4,x:32:-16,y:48:-16"
 decoder = UTIL.BitField64(encoding)
 
+
 # Command-line options
 def options() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", help="Particle type to analyze (default: %(default)s)", choices=["ne", "pi"], default="ne")
-    parser.add_argument("-n", help="Maximum number of events to analyze", default=0, type=int)
-    parser.add_argument("-p", help="Parquet file to read and/or write (default: %(default)s)", default="detailedHadrons.parquet")
-    parser.add_argument("-l", help="Load parquet file instead of lcio", action="store_true")
+    parser.add_argument(
+        "-i",
+        help="Particle type to analyze (default: %(default)s)",
+        choices=["ne", "pi"],
+        default="ne",
+    )
+    parser.add_argument(
+        "-n", help="Maximum number of events to analyze", default=0, type=int
+    )
+    parser.add_argument(
+        "-p",
+        help="Parquet file to read and/or write (default: %(default)s)",
+        default="detailedHadrons.parquet",
+    )
+    parser.add_argument(
+        "-l", help="Load parquet file instead of lcio", action="store_true"
+    )
     return parser.parse_args()
+
 
 @dataclass(frozen=True)
 class systems:
@@ -43,17 +59,16 @@ class systems:
     hcal_endcap = 11
     yoke_barrel = 13
     yoke_endcap = 14
-    ecal = [ecal_barrel,
-            ecal_endcap]
-    hcal = [hcal_barrel,
-            hcal_endcap]
-    yoke = [yoke_barrel,
-            yoke_endcap]
+    ecal = [ecal_barrel, ecal_endcap]
+    hcal = [hcal_barrel, hcal_endcap]
+    yoke = [yoke_barrel, yoke_endcap]
+
 
 @dataclass(frozen=True)
 class ppd:
     mipPe = 15.0
     npix = 2000.0
+
 
 @dataclass(frozen=True)
 class calib:
@@ -61,25 +76,31 @@ class calib:
         ecal = 0.0001575
         hcal_barrel = 0.0004925
         hcal_endcap = 0.0004725
+
     class mip_to_reco:
         ecal = 0.0066150
         hcal_barrel = 0.024625
         hcal_endcap = 0.024625
 
+
 @dataclass(frozen=True)
 class sampling_scaling:
-    ecal        = calib.mip_to_reco.ecal / calib.mip.ecal
+    ecal = calib.mip_to_reco.ecal / calib.mip.ecal
     hcal_barrel = calib.mip_to_reco.hcal_barrel / calib.mip.hcal_barrel
     hcal_endcap = calib.mip_to_reco.hcal_endcap / calib.mip.hcal_endcap
+
 
 def theta(x: float, y: float, z: float) -> float:
     return np.arccos(z / np.linalg.norm([x, y, z]))
 
+
 def eta(x: float, y: float, z: float) -> float:
-    return -np.log(np.tan(theta(x, y, z)/2.0))
+    return -np.log(np.tan(theta(x, y, z) / 2.0))
+
 
 def phi(x: float, y: float) -> float:
     return np.arctan2(y, x)
+
 
 @dataclass
 class particleFromTheGun:
@@ -91,6 +112,7 @@ class particleFromTheGun:
     def __post_init__(self):
         self.eta = eta(self.px, self.py, self.pz)
         self.phi = phi(self.px, self.py)
+
 
 @dataclass
 class particleReconstructed:
@@ -114,20 +136,31 @@ class particleReconstructed:
 
 # Set up things for each object
 settings = {
-    "fnames": { "ne": "/data/fmeloni/DataMuC_MuColl10_v0A/v2/reco/neutronGun_E_250_1000*"
-            },
-    "pdgid":  { "ne": [2112],
-                "pi": [211, 111],
-            },
+    "fnames": {
+        "ne": "/data/fmeloni/DataMuC_MuColl10_v0A/v2/reco/neutronGun_E_250_1000*"
+    },
+    "pdgid": {
+        "ne": [2112],
+        "pi": [211, 111],
+    },
 }
+
 
 # Progress bar
 def progress(time_diff, nprocessed, ntotal):
     nprocessed, ntotal = float(nprocessed), float(ntotal)
-    rate = (nprocessed+1)/time_diff
+    rate = (nprocessed + 1) / time_diff
     msg = "\r > %6i / %6i | %2i%% | %8.4fHz | %6.1fm elapsed | %6.1fm remaining"
-    msg = msg % (nprocessed, ntotal, 100*nprocessed/ntotal, rate, time_diff/60, (ntotal-nprocessed)/(rate*60))
+    msg = msg % (
+        nprocessed,
+        ntotal,
+        100 * nprocessed / ntotal,
+        rate,
+        time_diff / 60,
+        (ntotal - nprocessed) / (rate * 60),
+    )
     print(msg)
+
 
 # Script
 def main() -> None:
@@ -138,10 +171,13 @@ def main() -> None:
         study.write_data()
     study.plot_energy()
 
+
 # Analysis class
 class DetailedHadronStudy:
 
-    def __init__(self, obj_type: str, parquet_name: str, load_parquet: bool, num_events: int) -> None:
+    def __init__(
+        self, obj_type: str, parquet_name: str, load_parquet: bool, num_events: int
+    ) -> None:
         self.obj_type = obj_type
         self.parquet_name = parquet_name
         self.load_parquet = load_parquet
@@ -183,7 +219,6 @@ class DetailedHadronStudy:
             "pfx": {},
         }
 
-
     def load_data(self) -> None:
         if self.load_parquet:
             if self.num_events:
@@ -194,10 +229,8 @@ class DetailedHadronStudy:
             self.df = pd.DataFrame([vars(p) for p in particles])
         print(self.df)
 
-
     def write_data(self) -> None:
         self.df.to_parquet(self.parquet_name)
-
 
     def plot_energy(self) -> None:
         print("Plotting energy ... ")
@@ -208,27 +241,45 @@ class DetailedHadronStudy:
         with PdfPages("energy.pdf") as pdf:
             for source in ["sim", "dig", "rec", "clu", "pfo", "clx", "pfx"]:
                 for eta_min, eta_max in ETAS:
-                    print(f"Plotting {source} energy in eta range {eta_min}, {eta_max} ... ")
+                    print(
+                        f"Plotting {source} energy in eta range {eta_min}, {eta_max} ... "
+                    )
                     fig, ax = plt.subplots(figsize=(5, 4))
-                    condition = (abs(self.df.tru_eta) > eta_min) & (abs(self.df.tru_eta) < eta_max)
-                    counts, xedges, yedges, im = ax.hist2d(self.df.tru_e[condition],
-                                                           self.df[f"{source}_e"][condition],
-                                                           bins=(binsx, binsy),
-                                                           cmap="rainbow",
-                                                           cmin=0.1)
+                    condition = (abs(self.df.tru_eta) > eta_min) & (
+                        abs(self.df.tru_eta) < eta_max
+                    )
+                    counts, xedges, yedges, im = ax.hist2d(
+                        self.df.tru_e[condition],
+                        self.df[f"{source}_e"][condition],
+                        bins=(binsx, binsy),
+                        cmap="rainbow",
+                        cmin=0.1,
+                    )
                     ax.plot(linex, liney, color="gray", linewidth=1, linestyle="dashed")
                     ax.set_xlabel("True energy [GeV]")
                     ax.set_ylabel(f"{self.mapping[source]} energy [GeV]")
                     ax.tick_params(top=True, right=True)
-                    ax.text(0.05, 0.6, f"{eta_min} < |${{\eta}}$| < {eta_max}", transform=ax.transAxes, fontsize=12)
+                    ax.text(
+                        0.05,
+                        0.6,
+                        f"{eta_min} < |${{\eta}}$| < {eta_max}",
+                        transform=ax.transAxes,
+                        fontsize=12,
+                    )
                     for it, (label, value) in enumerate(self.constants[source].items()):
-                        ax.text(0.05, 0.95 - it*0.04, f"{label} = {value}", transform=ax.transAxes, fontsize=6, color="gray")
+                        ax.text(
+                            0.05,
+                            0.95 - it * 0.04,
+                            f"{label} = {value}",
+                            transform=ax.transAxes,
+                            fontsize=6,
+                            color="gray",
+                        )
                     cbar = fig.colorbar(im, ax=ax)
                     cbar.set_label("Number of entries")
                     fig.subplots_adjust(bottom=0.14, left=0.15, right=0.95, top=0.95)
                     pdf.savefig()
                     plt.close()
-
 
     def filenames(self) -> List[str]:
         samples = glob.glob(settings["fnames"][self.obj_type])
@@ -237,7 +288,6 @@ class DetailedHadronStudy:
             fnames += glob.glob(f"{s}/*.slcio")
         print("Found %i files." % len(fnames))
         return fnames
-
 
     def read_lcio(self) -> List[particleReconstructed]:
 
@@ -281,10 +331,13 @@ class DetailedHadronStudy:
                 tru_e, tru_n = 0, 0
                 for mcp in mcpCollection:
                     p, e = mcp.getMomentum(), mcp.getEnergy()
-                    if all([abs(mcp.getPDG()) in settings['pdgid'][self.obj_type],
-                            mcp.getGeneratorStatus()==1,
+                    if all(
+                        [
+                            abs(mcp.getPDG()) in settings["pdgid"][self.obj_type],
+                            mcp.getGeneratorStatus() == 1,
                             abs(eta(p[0], p[1], p[2])) < 2.0,
-                        ]):
+                        ]
+                    ):
                         tru_n += 1
                         p, e = mcp.getMomentum(), mcp.getEnergy()
                         particleTrue = particleFromTheGun(p[0], p[1], p[2], e)
@@ -307,36 +360,51 @@ class DetailedHadronStudy:
 
                 # Count number of items
                 def countElements(event, collection_names):
-                    return sum([len(getCollection(event, name)) for name in collection_names])
-                sim_n = countElements(event, ["ECalBarrelCollection",
-                                              "ECalEndcapCollection",
-                                              "HCalBarrelCollection",
-                                              "HCalEndcapCollection"])
-                dig_n = countElements(event, ["EcalBarrelCollectionDigi",
-                                              "EcalEndcapCollectionDigi",
-                                              "HcalBarrelCollectionDigi",
-                                              "HcalEndcapCollectionDigi"])
-                rec_n = countElements(event, ["EcalBarrelCollectionRec",
-                                              "EcalEndcapCollectionRec",
-                                              "HcalBarrelCollectionRec",
-                                              "HcalEndcapCollectionRec"])
+                    return sum(
+                        [len(getCollection(event, name)) for name in collection_names]
+                    )
+
+                sim_n = countElements(
+                    event,
+                    [
+                        "ECalBarrelCollection",
+                        "ECalEndcapCollection",
+                        "HCalBarrelCollection",
+                        "HCalEndcapCollection",
+                    ],
+                )
+                dig_n = countElements(
+                    event,
+                    [
+                        "EcalBarrelCollectionDigi",
+                        "EcalEndcapCollectionDigi",
+                        "HcalBarrelCollectionDigi",
+                        "HcalEndcapCollectionDigi",
+                    ],
+                )
+                rec_n = countElements(
+                    event,
+                    [
+                        "EcalBarrelCollectionRec",
+                        "EcalEndcapCollectionRec",
+                        "HcalBarrelCollectionRec",
+                        "HcalEndcapCollectionRec",
+                    ],
+                )
 
                 # Create a summary
                 tru_eta, tru_phi = particleTrue.eta, particleTrue.phi
                 particleSummary = particleReconstructed(
                     tru_eta,
                     tru_phi,
-
                     tru_e,
                     sim_e,
                     rec_e,
                     dig_e,
                     clu_e,
                     pfo_e,
-
                     clx_e,
                     pfx_e,
-
                     tru_n,
                     sim_n,
                     dig_n,
@@ -352,30 +420,34 @@ class DetailedHadronStudy:
 
         return particles
 
+
 def reconstructHcalEnergy(hit):
     """
     https://www.desy.de/~dudarboh/marlinreco_doc/html/RealisticCaloDigiScinPpd_8cc_source.html
     """
-    decoder.setValue((hit.getCellID0() & 0xffffffff) |
-                     (hit.getCellID1() << 32))
+    decoder.setValue((hit.getCellID0() & 0xFFFFFFFF) | (hit.getCellID1() << 32))
     energy = hit.getEnergy()
-    system = decoder['system'].value()
+    system = decoder["system"].value()
     assert system in systems.hcal
     barrel = system == systems.hcal_barrel
-    calibrCoeff = calib.mip_to_reco.hcal_barrel if barrel else calib.mip_to_reco.hcal_endcap
+    calibrCoeff = (
+        calib.mip_to_reco.hcal_barrel if barrel else calib.mip_to_reco.hcal_endcap
+    )
     r = 0.95
-    if (energy < r*ppd.npix):
-        energy = -ppd.npix * math.log ( 1. - ( energy / ppd.npix ) )
+    if energy < r * ppd.npix:
+        energy = -ppd.npix * math.log(1.0 - (energy / ppd.npix))
     else:
-        energy = 1/(1-r)*(energy-r*ppd.npix)-ppd.npix*math.log(1-r)
+        energy = 1 / (1 - r) * (energy - r * ppd.npix) - ppd.npix * math.log(1 - r)
     energy /= ppd.mipPe
     energy *= calibrCoeff
     return energy
+
 
 def getCollection(event, name):
     if name in event.getCollectionNames():
         return event.getCollection(name)
     return []
+
 
 def getEnergySim(event):
     energy = 0
@@ -384,14 +456,15 @@ def getEnergySim(event):
     hcal_b = getCollection(event, "HCalBarrelCollection")
     hcal_e = getCollection(event, "HCalEndcapCollection")
     for col, scaling in [
-            (ecal_b, sampling_scaling.ecal),
-            (ecal_e, sampling_scaling.ecal),
-            (hcal_b, sampling_scaling.hcal_barrel),
-            (hcal_e, sampling_scaling.hcal_endcap),
+        (ecal_b, sampling_scaling.ecal),
+        (ecal_e, sampling_scaling.ecal),
+        (hcal_b, sampling_scaling.hcal_barrel),
+        (hcal_e, sampling_scaling.hcal_endcap),
     ]:
         for sim in col:
-            energy += sim.getEnergy()*scaling
+            energy += sim.getEnergy() * scaling
     return energy
+
 
 def getEnergyDig(event):
     energy = 0
@@ -401,11 +474,12 @@ def getEnergyDig(event):
     hcal_e = getCollection(event, "HcalEndcapCollectionDigi")
     for col in [ecal_b, ecal_e]:
         for dig in col:
-            energy += dig.getEnergy()*calib.mip_to_reco.ecal
+            energy += dig.getEnergy() * calib.mip_to_reco.ecal
     for col in [hcal_b, hcal_e]:
         for dig in col:
             energy += reconstructHcalEnergy(dig)
     return energy
+
 
 def getEnergyRec(event):
     energy = 0
@@ -418,6 +492,7 @@ def getEnergyRec(event):
             energy += rec.getEnergy()
     return energy
 
+
 def getEnergyAndNumberClu(event):
     energy_lead, energy_all, num = 0, 0, 0
     cluCollection = getCollection(event, "PandoraClusters")
@@ -426,10 +501,15 @@ def getEnergyAndNumberClu(event):
         return 0, 0, 0
     return max(energies), sum(energies), len(energies)
 
+
 def getEnergyAndNumberPfo(event, obj_type):
     energy, num = 0, 0
     pfos = getCollection(event, "PandoraPFOs")
-    energies = [pfo.getEnergy() for pfo in pfos if abs(pfo.getType()) in settings['pdgid'][obj_type]]
+    energies = [
+        pfo.getEnergy()
+        for pfo in pfos
+        if abs(pfo.getType()) in settings["pdgid"][obj_type]
+    ]
     if len(energies) == 0:
         return 0, 0, 0
     return max(energies), sum(energies), len(energies)
@@ -439,6 +519,7 @@ def getEnergyAndNumberPfo(event, obj_type):
     #         if pfo.getEnergy() > energy:
     #             energy = pfo.getEnergy()
     # return energy, num
+
 
 if __name__ == "__main__":
     main()
