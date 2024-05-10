3bfc00e34b3eb59be6847db17f11866640ca043b
Alexander Tuna <alexander.tuna@gmail.com>
2024-05-09 23:46:38 -0500

    One less indentation

--- compare_mcp_sim/main.py
+++ compare_mcp_sim/main.py
@@ -1,6 +1,7 @@
 import pyLCIO
 from pyLCIO import EVENT, UTIL
 
+
 def main():
 
     fname = "/data/fmeloni/DataMuC_MuColl10_v0A/v2/reco/neutronGun_E_250_1000/neutronGun_E_250_1000_reco_10000.slcio"
@@ -16,28 +17,37 @@ def main():
         print(f"len(hcals_b): {len(hcals_b)}")
         print(f"len(hcals_e): {len(hcals_e)}")
 
-        # print all MCP
-        if i_event == 2:
-            for i_mcp, mcp in enumerate(mcps):
-                print(i_mcp, pretty(mcp.getVertex(), mcp.getEnergy(), mcp.getTime()))
-
-        # x, y, z, E, t
-        if i_event == 2:
-            for i_hit, hit in enumerate(hcals_b):
-                print("*", i_event, pretty(hit.getPosition(), hit.getEnergy(), 0))
-                for i_mcp in range(hit.getNMCContributions()):
-                    print("-", i_mcp, pretty(hit.getStepPosition(i_mcp),
-                                             hit.getEnergyCont(i_mcp),
-                                             hit.getTimeCont(i_mcp)))
-                print("")
-                if i_hit > 10:
-                    break
         if i_event > 2:
             break
+        if i_event != 2:
+            continue
+
+        # print all MCP
+        for i_mcp, mcp in enumerate(mcps):
+            print(i_mcp, pretty(mcp.getVertex(), mcp.getEnergy(), mcp.getTime()))
+
+        # x, y, z, E, t
+        for i_hit, hit in enumerate(hcals_b):
+            print("*", i_event, pretty(hit.getPosition(), hit.getEnergy(), 0))
+            for i_mcp in range(hit.getNMCContributions()):
+                print(
+                    "-",
+                    i_mcp,
+                    pretty(
+                        hit.getStepPosition(i_mcp),
+                        hit.getEnergyCont(i_mcp),
+                        hit.getTimeCont(i_mcp),
+                    ),
+                )
+            print("")
+            if i_hit > 10:
+                break
+
 
 def pretty(pos, e, t):
     x, y, z = pos[0], pos[1], pos[2]
     return f"x={int(x):5}, y={int(y):5}, z={int(z):5}, E={e:.8f}, t={int(t)}"
+
 
 def getCollection(event, name):
     if name in event.getCollectionNames():
