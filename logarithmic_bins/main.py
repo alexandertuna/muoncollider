b4d96006c175fff28819eb81215cf4bc7c89d7ea
Alexander Tuna <alexander.tuna@gmail.com>
2024-05-13 00:15:36 -0500

    Start log bins slides

---
+++ logarithmic_bins/main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+mpl.use("Agg")
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+def main():
+    print("hello")
+
+if __name__ == "__main__":
+    main()
