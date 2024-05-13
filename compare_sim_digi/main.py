fd9f09ea084c2e51e481b630771057abf44d5b84
Alexander Tuna <alexander.tuna@gmail.com>
2024-05-12 22:34:58 -0500

    Make endcap work. Cleanup and speedup.

--- compare_sim_digi/main.py
+++ compare_sim_digi/main.py
@@ -13,8 +13,10 @@ https://github.com/madbaron/SteeringMacros/blob/master/k4Reco/steer_reco_CONDOR.
 import pyLCIO # type: ignore
 from pyLCIO import EVENT, UTIL
 
+from dataclasses import dataclass
 import math
 import os
+
 from numpy.random import poisson
 from numpy.random import binomial
 import numpy as np
@@ -26,7 +28,7 @@ from matplotlib.backends.backend_pdf import PdfPages
 
 FNAMES = (
     "/data/fmeloni/DataMuC_MuColl10_v0A/v2/reco/neutronGun_E_250_1000/neutronGun_E_250_1000_reco_10000.slcio",
-    # "/data/fmeloni/DataMuC_MuColl10_v0A/v2/reco/neutronGun_E_250_1000/neutronGun_E_250_1000_reco_10100.slcio",
+    "/data/fmeloni/DataMuC_MuColl10_v0A/v2/reco/neutronGun_E_250_1000/neutronGun_E_250_1000_reco_10100.slcio",
     # "/data/fmeloni/DataMuC_MuColl10_v0A/v2/reco/neutronGun_E_250_1000/neutronGun_E_250_1000_reco_10200.slcio",
     # "/data/fmeloni/DataMuC_MuColl10_v0A/v2/reco/neutronGun_E_250_1000/neutronGun_E_250_1000_reco_10300.slcio",
     # "/data/fmeloni/DataMuC_MuColl10_v0A/v2/reco/neutronGun_E_250_1000/neutronGun_E_250_1000_reco_10400.slcio",
@@ -41,31 +43,195 @@ MIP = 0
 GEVDEP = 1
 NPE = 2
 
+
 def main() -> None:
 
-    parquet_name = "compare_sim_digi.parquet"
-
-    study = SimDigiComparison()
-    for fname in FNAMES:
-        study.analyze(fname)
-
-    print("Converting dict to pandas.DataFrame ...")
-    df = pd.DataFrame(study.data)
-
-    print(f"Writing {parquet_name} ...")
-    df.to_parquet(parquet_name)
-
+    study = SimDigiComparison(FNAMES)
+    study.analyze()
+    study.write_dataframe()
     study.plot()
 
 
+@dataclass(frozen=True)
 class systems:
+
     ecal_barrel = 20
-    ecal_endcap = 21
+    ecal_endcap = 29
     hcal_barrel = 10
     hcal_endcap = 11
+    yoke_barrel = 13
+    yoke_endcap = 14
     ecal = [ecal_barrel, ecal_endcap]
     hcal = [hcal_barrel, hcal_endcap]
+    yoke = [yoke_barrel, yoke_endcap]
 
+
+class SimDigiComparison:
+
+    def __init__(self, fnames: list[str]) -> None:
+        self.fnames = fnames
+        self.data = self.default_dict()
+        self.parquet_name = "compare_sim_digi.parquet"
+
+
+    def default_dict(self):
+        return {
+            "n_mc_contributions": [],
+            "system": [],
+            "digit_energy_manual": [],
+            "digit_energy_pandora": [],
+            "digit_time_manual": [],
+            "digit_time_pandora": [],
+            "digit_exists_manual": [],
+            "digit_exists_pandora": [],
+        }
+
+
+    def analyze(self) -> None:
+        self.data = self.default_dict()
+        for fname in self.fnames:
+            data = self.analyze_file(fname)
+            for key in data:
+                self.data[key].extend(data[key])
+        print("Converting dict to pandas.DataFrame ...")
+        self.df = pd.DataFrame(self.data)
+
+
+    def write_dataframe(self) -> None:
+        print(f"Writing {self.parquet_name} ...")
+        self.df.to_parquet(self.parquet_name)
+
+
+    def analyze_file(self, fname: str) -> None:
+        print(f"Analyzing {fname} ...")
+
+        data = self.default_dict()
+        reader = pyLCIO.IOIMPL.LCFactory.getInstance().createLCReader()
+        reader.open(fname)
+
+        for i_event, event in enumerate(reader):
+
+            # events_of_interest = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+            # # events_of_interest = range(1000)
+            # if i_event not in events_of_interest:
+            #     continue
+            # if i_event > max(events_of_interest):
+            #     break
+
+            print(f"Event {i_event}")
+
+            for calo, part in [
+                    ("E", "Barrel"),
+                    ("E", "Endcap"),
+                    ("H", "Barrel"),
+                    ("H", "Endcap"),
+            ]:
+                colname = f"{calo}Cal{part}Collection"
+                try:
+                    col = event.getCollection(colname)
+                    sim2digit = {rel.getTo(): rel.getFrom() for rel in event.getCollection(f"{calo}cal{part}RelationsSimDigi")}
+                except:
+                    continue
+
+                for i_sim, sim in enumerate(col):
+                    digit_exp = DigitizedHit(sim)
+                    digit_energy, digit_time = -1, -1
+                    if sim in sim2digit:
+                        digit_energy, digit_time = sim2digit[sim].getEnergy(), sim2digit[sim].getTime()
+
+                    for name, var in [
+                            ("n_mc_contributions", sim.getNMCContributions()),
+                            ("system", digit_exp.system),
+                            ("digit_energy_manual", digit_exp.energy),
+                            ("digit_energy_pandora", digit_energy),
+                            ("digit_time_manual", digit_exp.time),
+                            ("digit_time_pandora", digit_time),
+                            ("digit_exists_manual", digit_exp.passed),
+                            ("digit_exists_pandora", sim in sim2digit),
+                    ]:
+                        data[name].append(var)
+
+        print("Closing ...")
+        reader.close()
+        return data
+
+
+    def human_readable(self, system):
+        if system == systems.hcal_barrel:
+            return "HCal Barrel"
+        elif system == systems.hcal_endcap:
+            return "HCal Endcap"
+        elif system == systems.ecal_barrel:
+            return "ECal Barrel"
+        elif system == systems.ecal_endcap:
+            return "ECal Endcap"
+        raise Exception(f"Unknown system: {system}")
+
+
+    def unit(self, var, system):
+        if var == "energy":
+            if system in systems.ecal:
+                return "MIP"
+            else:
+                return "NPE"
+        elif var == "time":
+            return "ns"
+        raise Exception(f"Unknown variable: {var}")
+
+
+    def plot(self):
+        print(f"Reading {self.parquet_name} ...")
+        df = pd.read_parquet(self.parquet_name)
+        print(df)
+
+        with PdfPages("digitization.pdf") as pdf:
+            for system in systems.ecal + systems.hcal:
+                for var in ["energy", "time"]:
+
+                    condition = (df.system == system) & df.digit_exists_manual & df.digit_exists_pandora
+                    subset = df[condition]
+                    manual = subset[f"digit_{var}_manual"]
+                    pandora = subset[f"digit_{var}_pandora"]
+                    if var == "energy" and system in systems.hcal:
+                        pandora = pandora + np.random.rand(len(pandora)) - 0.5
+
+                    # 1D comparison
+                    fig, ax = plt.subplots(figsize=(4, 4))
+                    bins = np.linspace(-0.5, 0.5, 200)
+                    ax.hist((manual - pandora) / pandora, bins=bins)
+                    ax.set_xlabel(f"Digit {var}: (by hand - pandora) / pandora")
+                    ax.set_ylabel(f"Number of hits")
+                    ax.tick_params(top=True, right=True)
+                    ax.text(0.08, 0.8, self.human_readable(system), transform=ax.transAxes)
+                    fig.subplots_adjust(bottom=0.14, left=0.20, right=0.95, top=0.95)
+                    pdf.savefig()
+                    plt.close()
+
+                    # 2D comparison
+                    fig, ax = plt.subplots(figsize=(4, 4))
+                    the_unit = self.unit(var, system)
+                    min_val = min([pandora.min(), manual.min()])
+                    max_val = max([pandora.max(), manual.max()])
+                    linex = liney = [min_val, max_val]
+                    if var == "energy":
+                        bins = np.logspace(math.log(min_val, 10), math.log(max_val, 10), 100)
+                    else:
+                        bins = np.linspace(min_val, max_val, 100)
+                    counts, xedges, yedges, im = ax.hist2d(pandora, manual, bins=[bins, bins], cmap="rainbow", cmin=1e-7)
+                    ax.set_xlabel(f"Digit {var} [{the_unit}] (pandora)")
+                    ax.set_ylabel(f"Digit {var} [{the_unit}] (by hand)")
+                    ax.tick_params(top=True, right=True)
+                    if var == "energy":
+                        ax.semilogx()
+                        ax.semilogy()
+                    cbar = fig.colorbar(im, ax=ax)
+                    cbar.set_label("Number of hits")
+                    ax.text(0.08, 0.8, self.human_readable(system), transform=ax.transAxes)
+                    # ax.plot(linex, liney, color="gray", linewidth=1, linestyle="dashed")
+                    fig.subplots_adjust(bottom=0.14, left=0.15, right=0.85, top=0.95)
+                    pdf.savefig()
+                    plt.close()
+                
 
 class DigitizedHit:
 
@@ -75,6 +241,12 @@ class DigitizedHit:
         self.time, self.energy_ = self.StandardIntegration()
         self.energy = self.EnergyDigi(self.energy_)
         self.passed = self.energy > self.threshold()
+        if self.system not in systems.ecal + systems.hcal:
+            raise Exception(f"Unknown system: {self.system}")
+
+
+    def is_yoke(self):
+        return self.system in systems.yoke
 
 
     def StandardIntegration(self) -> [float, float]:
@@ -157,7 +329,7 @@ class DigitizedHit:
 
     def digitiseDetectorEnergy_RealisticCaloDigiScinPpd(self, energy: float) -> float:
         _PPD_pe_per_mip = 15.0
-        _calib_mip = 0.0004925 # barrel ### 0.0004725 endcap
+        _calib_mip = 0.0004925 if self.system == systems.hcal_barrel else 0.0004725
         _PPD_n_pixels = 2000.0
         _pixSpread = 0.0
         npe = energy * _PPD_pe_per_mip / _calib_mip
@@ -213,107 +385,6 @@ class DigitizedHit:
         return self.convertEnergy_RealisticCaloDigiScinPpd(_threshold_value, _threshold_iunit)
 
 
-class SimDigiComparison:
-
-
-    def __init__(self):
-        self.data = {
-            "n_mc_contributions": [],
-            "system": [],
-            "digit_energy_manual": [],
-            "digit_energy_pandora": [],
-            "digit_time_manual": [],
-            "digit_time_pandora": [],
-            "digit_exists_manual": [],
-            "digit_exists_pandora": [],
-        }
-        self.parquet_name = "compare_sim_digi.parquet"
-
-
-    def analyze(self, fname: str) -> None:
-        print(f"Analyzing {fname} ...")
-        reader = pyLCIO.IOIMPL.LCFactory.getInstance().createLCReader()
-        reader.open(fname)
-        for i_event, event in enumerate(reader):
-
-            event_of_interest = [0, 1, 2, 3, 4, 5, 6]
-            # event_of_interest = range(1000)
-            if i_event not in event_of_interest:
-                continue
-            if i_event > max(event_of_interest):
-                break
-
-            print(f"Event {i_event}")
-
-            for calo in ["E", "H"]:
-
-                colname = f"{calo}CalBarrelCollection"
-                try:
-                    col = event.getCollection(colname)
-                    relations_sim = [el.getTo()   for el in event.getCollection(f"{calo}calBarrelRelationsSimDigi")]
-                    relations_dig = [el.getFrom() for el in event.getCollection(f"{calo}calBarrelRelationsSimDigi")]
-                except:
-                    continue
-
-                for i_el, el in enumerate(col):
-                    digit_exp = DigitizedHit(el)
-                    digit_energy, digit_time = -1, -1
-                    if el in relations_sim:
-                        index = relations_sim.index(el)
-                        digit_energy, digit_time = relations_dig[index].getEnergy(), relations_dig[index].getTime()
-
-                    for name, var in [
-                            ("n_mc_contributions", el.getNMCContributions()),
-                            ("system", digit_exp.system),
-                            ("digit_energy_manual", digit_exp.energy),
-                            ("digit_energy_pandora", digit_energy),
-                            ("digit_time_manual", digit_exp.time),
-                            ("digit_time_pandora", digit_time),
-                            ("digit_exists_manual", digit_exp.passed),
-                            ("digit_exists_pandora", el in relations_sim),
-                    ]:
-                        self.data[name].append(var)
-
-
-    def plot(self):
-        print(f"Reading {self.parquet_name} ...")
-        df = pd.read_parquet(self.parquet_name)
-        print(df)
-
-        with PdfPages("digitization.pdf") as pdf:
-            for system in [10, 20]:
-                for var in ["energy", "time"]:
-
-                    condition = (df.system == system) & df.digit_exists_manual & df.digit_exists_pandora
-                    subset = df[condition]
-
-                    # 1D comparison
-                    fig, ax = plt.subplots(figsize=(4, 4))
-                    bins = np.linspace(-0.5, 0.5, 200)
-                    ax.hist((subset[f"digit_{var}_manual"] - subset[f"digit_{var}_pandora"]) / subset[f"digit_{var}_pandora"], bins=bins)
-                    ax.set_xlabel(f"System {system} {var} (byhand - pandora) / pandora")
-                    ax.tick_params(top=True, right=True)
-                    pdf.savefig()
-                    plt.close()
-
-                    # 2D comparison
-                    fig, ax = plt.subplots(figsize=(4, 4))
-                    min_val = min([subset[f"digit_{var}_pandora"].min(), subset[f"digit_{var}_manual"].min()])
-                    max_val = max([subset[f"digit_{var}_pandora"].max(), subset[f"digit_{var}_manual"].max()])
-                    if var == "energy":
-                        bins = np.logspace(math.log(min_val, 10), math.log(max_val, 10), 100)
-                    else:
-                        bins = np.linspace(min_val, max_val, 100)
-                    ax.hist2d(subset[f"digit_{var}_pandora"], subset[f"digit_{var}_manual"], bins=[bins, bins], cmap="rainbow", cmin=1e-7)
-                    ax.set_xlabel(f"System {system} {var} pandora")
-                    ax.set_ylabel(f"System {system} {var} byhand")
-                    ax.tick_params(top=True, right=True)
-                    if var == "energy":
-                        ax.semilogx()
-                        ax.semilogy()
-                    pdf.savefig()
-                    plt.close()
-                
 
 if __name__ == "__main__":
     main()
