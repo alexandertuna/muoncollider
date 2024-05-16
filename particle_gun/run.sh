bd1ca7e260006d653c2cb68f8e34aca236868e4c
Alexander Tuna <alexander.tuna@gmail.com>
2024-05-16 17:39:39 -0500

    Using the latest geo

--- particle_gun/run.sh
+++ particle_gun/run.sh
@@ -6,6 +6,8 @@ PHI=0
 LCIO="pgun_neutron.slcio"
 SIM="pgun_neutron.sim.slcio"
 
+export MUCOLL_GEO="/work/tuna/detector-simulation/geometries/MuColl_10TeV_v0A/MuColl_10TeV_v0A.xml"
+
 time python ../mucoll-benchmarks/generation/pgun/pgun_lcio.py -e ${EVENTS} -p 1 --pdg ${PARTICLE} --p ${ENERGY} --theta ${THETA} --phi ${PHI} -- ${LCIO}
 
 time ddsim --steeringFile ../mucoll-benchmarks/simulation/ilcsoft/steer_baseline.py --inputFile ${LCIO} --outputFile ${SIM}
