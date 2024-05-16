55a5aed85e107c4f9ae776de10b4f859781306bd
Alexander Tuna <alexander.tuna@gmail.com>
2024-05-16 17:44:17 -0500

    Blackened

--- calo_lcio_to_pandas/main.py
+++ calo_lcio_to_pandas/main.py
@@ -1,4 +1,4 @@
-import pyLCIO # type: ignore
+import pyLCIO  # type: ignore
 from pyLCIO import EVENT, UTIL
 
 import argparse
@@ -12,8 +12,9 @@ from typing import Any, List, Tuple
 
 
 def options() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser(usage=__doc__,
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(
+        usage=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
     parser.add_argument("-i", help="Comma-separated input filenames")
     return parser.parse_args()
 
@@ -21,9 +22,9 @@ def options() -> argparse.ArgumentParser:
 def main() -> None:
     ops = options()
     if not ops.i:
-        raise Exception('Need input file with -i')
-    fnames = ops.i.split(',')
-    pqname = 'writeCaloHits.parquet'
+        raise Exception("Need input file with -i")
+    fnames = ops.i.split(",")
+    pqname = "writeCaloHits.parquet"
     writer = CaloHitWriter(fnames, pqname)
     writer.read_hits()
     writer.write_hits()
@@ -36,13 +37,11 @@ class CaloHitWriter:
         self.pqname = pqname
         self.df = self.default_dict()
 
-
     def read_hits(self) -> None:
         n_workers = 10
         with mp.Pool(n_workers) as pool:
             results = pool.map(self.read_hits_serially, self.fnames)
         self.df = pd.DataFrame(self.merge_results(results))
-
 
     def merge_results(self, results: List[dict]) -> dict:
         print("Merging dicts ...")
@@ -52,7 +51,6 @@ class CaloHitWriter:
                 result[key].extend(res[key])
         return result
 
-
     def read_hits_serially(self, fname: str) -> dict:
         reader = pyLCIO.IOIMPL.LCFactory.getInstance().createLCReader()
         reader.open(fname)
@@ -60,39 +58,36 @@ class CaloHitWriter:
         results = [self.processEventCellView(event) for event in reader]
         end = time.perf_counter()
         reader.close()
-        self.announceTime(end-start, len(results))
+        self.announceTime(end - start, len(results))
         return self.merge_results(results)
 
-
     def announceTime(self, duration: float, n: int) -> None:
-        rate = n/duration
-        print(f'Event rate: {rate:.3f} Hz ({n} events)')
-
+        rate = n / duration
+        print(f"Event rate: {rate:.3f} Hz ({n} events)")
 
     def default_dict(self) -> dict:
         return {
-            'event': [],
-            'hit_system': [],
-            'hit_side': [],
-            'hit_layer': [],
-            'hit_x': [],
-            'hit_y': [],
-            'hit_z': [],
-            'hit_e': [],
-            'truth_px': [],
-            'truth_py': [],
-            'truth_pz': [],
-            'truth_e': [],
+            "event": [],
+            "hit_system": [],
+            "hit_side": [],
+            "hit_layer": [],
+            "hit_x": [],
+            "hit_y": [],
+            "hit_z": [],
+            "hit_e": [],
+            "truth_px": [],
+            "truth_py": [],
+            "truth_pz": [],
+            "truth_e": [],
         }
-
 
     def processEventCellView(self, event: Any) -> dict:
         # print(f'On event {event}')
         colnames = [
-            'ECalBarrelCollection',
-            'ECalEndcapCollection',
-            'HCalBarrelCollection',
-            'HCalEndcapCollection',
+            "ECalBarrelCollection",
+            "ECalEndcapCollection",
+            "HCalBarrelCollection",
+            "HCalEndcapCollection",
         ]
         truth_px, truth_py, truth_pz, truth_e = self.processEventTruth(event)
         cols = [event.getCollection(name) for name in colnames]
@@ -101,27 +96,27 @@ class CaloHitWriter:
             cellIdEncoding = col.getParameters().getStringVal(EVENT.LCIO.CellIDEncoding)
             cellIdDecoder = UTIL.BitField64(cellIdEncoding)
             for i_hit, hit in enumerate(col):
-                cellIdDecoder.setValue((hit.getCellID0() & 0xffffffff) |
-                                       (hit.getCellID1() << 32))
+                cellIdDecoder.setValue(
+                    (hit.getCellID0() & 0xFFFFFFFF) | (hit.getCellID1() << 32)
+                )
                 position = hit.getPosition()
                 x, y, z = position[0], position[1], position[2]
-                d['event'].append(event.getEventNumber())
-                d['hit_system'].append(cellIdDecoder['system'].value())
-                d['hit_side'].append(cellIdDecoder['side'].value())
-                d['hit_layer'].append(cellIdDecoder['layer'].value())
-                d['hit_x'].append(x)
-                d['hit_y'].append(y)
-                d['hit_z'].append(z)
-                d['hit_e'].append(hit.getEnergy())
-                d['truth_px'].append(truth_px)
-                d['truth_py'].append(truth_py)
-                d['truth_pz'].append(truth_pz)
-                d['truth_e'].append(truth_e)
+                d["event"].append(event.getEventNumber())
+                d["hit_system"].append(cellIdDecoder["system"].value())
+                d["hit_side"].append(cellIdDecoder["side"].value())
+                d["hit_layer"].append(cellIdDecoder["layer"].value())
+                d["hit_x"].append(x)
+                d["hit_y"].append(y)
+                d["hit_z"].append(z)
+                d["hit_e"].append(hit.getEnergy())
+                d["truth_px"].append(truth_px)
+                d["truth_py"].append(truth_py)
+                d["truth_pz"].append(truth_pz)
+                d["truth_e"].append(truth_e)
         return d
 
-
     def processEventTruth(self, event: Any) -> Tuple[float, float, float, float]:
-        colname = 'MCParticle'
+        colname = "MCParticle"
         event_number = event.getEventNumber()
         n_stable = 0
         for obj in event.getCollection(colname):
@@ -132,15 +127,13 @@ class CaloHitWriter:
                 n_stable += 1
                 # break
         if n_stable != 1:
-            raise Exception('Unexpected truth particles')
+            raise Exception("Unexpected truth particles")
         return pxypyze
 
-
     def write_hits(self):
-        print(f'Writing hits to file: {self.df.shape}')
+        print(f"Writing hits to file: {self.df.shape}")
         self.df.to_parquet(self.pqname)
-        
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-
