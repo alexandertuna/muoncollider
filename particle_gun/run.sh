843bfc80af40e7b7574507703f6bca3657c8331b
Alexander Tuna <alexander.tuna@gmail.com>
2024-05-17 11:35:48 -0500

    Use || return to prevent script from trucking forward

--- particle_gun/run.sh
+++ particle_gun/run.sh
@@ -3,13 +3,18 @@ PARTICLE="2112"
 ENERGY="50 500"
 THETA=20
 PHI=0
-LCIO="pgun_neutron.slcio"
-SIM="pgun_neutron.sim.slcio"
+DATA="/work/tuna/data"
+LCIO="${DATA}/pgun_neutron.slcio"
+SIM="${DATA}/pgun_neutron.sim.slcio"
+CODE="/code"
 
-export MUCOLL_GEO="/work/tuna/detector-simulation/geometries/MuColl_10TeV_v0A/MuColl_10TeV_v0A.xml"
+export MUCOLL_GEO="${CODE}/detector-simulation/geometries/MuColl_10TeV_v0A/MuColl_10TeV_v0A.xml"
 
-time python ../mucoll-benchmarks/generation/pgun/pgun_lcio.py -e ${EVENTS} -p 1 --pdg ${PARTICLE} --p ${ENERGY} --theta ${THETA} --phi ${PHI} -- ${LCIO}
+time python ${CODE}/mucoll-benchmarks/generation/pgun/pgun_lcio.py -o -e ${EVENTS} -p 1 --pdg ${PARTICLE} --p ${ENERGY} --theta ${THETA} --phi ${PHI} -- ${LCIO} || return
 
-time ddsim --steeringFile ../mucoll-benchmarks/simulation/ilcsoft/steer_baseline.py --inputFile ${LCIO} --outputFile ${SIM}
+time ddsim --steeringFile ${CODE}/mucoll-benchmarks/simulation/ilcsoft/steer_baseline.py --inputFile ${LCIO} --outputFile ${SIM} || return
+# time ddsim --inputFile ${LCIO} --steeringFile /code/SteeringMacros/Sim/sim_steer_GEN_CONDOR.py --outputFile ${SIM}
 
-time Marlin --global.LCIOInputFiles=${SIM} --DD4hep.DD4hepXMLFile=${MUCOLL_GEO} ../mucoll-benchmarks/digitisation/marlin/digi_steer.xml
+time k4run ${CODE}/SteeringMacros/k4Reco/steer_reco_CONDOR.py || return
+# gaudirun.py ../SteeringMacros/k4Reco/steer_reco_CONDOR.py
+# time Marlin --global.LCIOInputFiles=${SIM} --DD4hep.DD4hepXMLFile=${MUCOLL_GEO} ../mucoll-benchmarks/digitisation/marlin/digi_steer.xml
