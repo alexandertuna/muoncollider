1fcb2f8978b1a352100867f1c1a2940c6e076ec2
Alexander Tuna <alexander.tuna@gmail.com>
2024-05-31 11:27:03 -0500

    Making this slightly more scalable

--- particle_gun/run.sh
+++ particle_gun/run.sh
@@ -1,20 +1,36 @@
-EVENTS="10"
-PARTICLE="2112"
+EVENTS="5"
+PARTICLE="photon"
+PDGID="22"
+# PARTICLE="neutron"
+# PDGID="2112"
 ENERGY="50 500"
 THETA=20
 PHI=0
-DATA="/work/tuna/data"
-LCIO="${DATA}/pgun_neutron.slcio"
-SIM="${DATA}/pgun_neutron.sim.slcio"
+
+NOW=$(date +%Y_%m_%d_%Hh%Mm%Ss)
+
 CODE="/code"
+TMP="/tmp/tuna/${NOW}"
+DATA="/work/tuna/data"
+
+GEN=${DATA}/${PARTICLE}/${PARTICLE}_gen_${NOW}.slcio
+SIM=${DATA}/${PARTICLE}/${PARTICLE}_sim_${NOW}.slcio
 
 export MUCOLL_GEO="${CODE}/detector-simulation/geometries/MuColl_10TeV_v0A/MuColl_10TeV_v0A.xml"
 
-time python ${CODE}/mucoll-benchmarks/generation/pgun/pgun_lcio.py -o -e ${EVENTS} -p 1 --pdg ${PARTICLE} --p ${ENERGY} --theta ${THETA} --phi ${PHI} -- ${LCIO} || return
+mkdir $TMP
+cd $TMP
 
-time ddsim --steeringFile ${CODE}/mucoll-benchmarks/simulation/ilcsoft/steer_baseline.py --inputFile ${LCIO} --outputFile ${SIM} || return
-# time ddsim --inputFile ${LCIO} --steeringFile /code/SteeringMacros/Sim/sim_steer_GEN_CONDOR.py --outputFile ${SIM}
+cp ${CODE}/SteeringMacros/k4Reco/steer_reco_CONDOR.py ./ || return
+sed -i "s/INFILENAME/${NOW}/g" steer_reco_CONDOR.py || return
+sed -i "s/TYPEEVENT/${PARTICLE}/g" steer_reco_CONDOR.py || return
 
-time k4run ${CODE}/SteeringMacros/k4Reco/steer_reco_CONDOR.py || return
-# gaudirun.py ../SteeringMacros/k4Reco/steer_reco_CONDOR.py
+time python ${CODE}/mucoll-benchmarks/generation/pgun/pgun_lcio.py -o -e ${EVENTS} -p 1 --pdg ${PDGID} --p ${ENERGY} --theta ${THETA} --phi ${PHI} -- ${GEN} || return
+
+time ddsim --steeringFile ${CODE}/mucoll-benchmarks/simulation/ilcsoft/steer_baseline.py --inputFile ${GEN} --outputFile ${SIM} || return
+# time ddsim --inputFile ${GEN} --steeringFile /code/SteeringMacros/Sim/sim_steer_GEN_CONDOR.py --outputFile ${SIM}
+
+time k4run ./steer_reco_CONDOR.py || return
+# time k4run ${CODE}/SteeringMacros/k4Reco/steer_reco_CONDOR.py || return
 # time Marlin --global.LCIOInputFiles=${SIM} --DD4hep.DD4hepXMLFile=${MUCOLL_GEO} ../mucoll-benchmarks/digitisation/marlin/digi_steer.xml
+
