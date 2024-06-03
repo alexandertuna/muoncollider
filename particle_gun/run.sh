2018ddee2b7046f550c33c561ddd101e3ba4d076
Alexander Tuna <alexander.tuna@gmail.com>
2024-06-03 15:24:33 -0500

    A couple more print statements

--- particle_gun/run.sh
+++ particle_gun/run.sh
@@ -1,4 +1,4 @@
-EVENTS="5"
+EVENTS="2000"
 PARTICLE="photon"
 PDGID="22"
 # PARTICLE="neutron"
@@ -18,13 +18,16 @@ SIM=${DATA}/${PARTICLE}/${PARTICLE}_sim_${NOW}.slcio
 
 export MUCOLL_GEO="${CODE}/detector-simulation/geometries/MuColl_10TeV_v0A/MuColl_10TeV_v0A.xml"
 
+echo "mkdir and cd"
 mkdir $TMP
 cd $TMP
 
+echo "cp and sed"
 cp ${CODE}/SteeringMacros/k4Reco/steer_reco_CONDOR.py ./ || return
 sed -i "s/INFILENAME/${NOW}/g" steer_reco_CONDOR.py || return
 sed -i "s/TYPEEVENT/${PARTICLE}/g" steer_reco_CONDOR.py || return
 
+echo "pgun"
 time python ${CODE}/mucoll-benchmarks/generation/pgun/pgun_lcio.py -o -e ${EVENTS} -p 1 --pdg ${PDGID} --p ${ENERGY} --theta ${THETA} --phi ${PHI} -- ${GEN} || return
 
 time ddsim --steeringFile ${CODE}/mucoll-benchmarks/simulation/ilcsoft/steer_baseline.py --inputFile ${GEN} --outputFile ${SIM} || return
@@ -34,3 +37,4 @@ time k4run ./steer_reco_CONDOR.py || return
 # time k4run ${CODE}/SteeringMacros/k4Reco/steer_reco_CONDOR.py || return
 # time Marlin --global.LCIOInputFiles=${SIM} --DD4hep.DD4hepXMLFile=${MUCOLL_GEO} ../mucoll-benchmarks/digitisation/marlin/digi_steer.xml
 
+echo "rm -f ${SIM}"
