8edc35d195fe161123940c09a5a558ca0ce84473
Alexander Tuna <alexander.tuna@gmail.com>
2024-06-18 12:40:42 -0500

    Mess with pi0s. Dont cd

--- particle_gun/run.sh
+++ particle_gun/run.sh
@@ -1,11 +1,14 @@
-EVENTS="2"
+EVENTS="5"
+# PARTICLE="pi0"
+# PDGID="111"
 PARTICLE="photon"
 PDGID="22"
 # PARTICLE="neutron"
 # PDGID="2112"
-ENERGY="50 500"
+# ENERGY="50 500"
+ENERGY="100"
 THETA=20
-PHI=0
+PHI=90
 
 NOW=$(date +%Y_%m_%d_%Hh%Mm%Ss)
 
@@ -15,17 +18,17 @@ DATA="/work/tuna/data"
 
 GEN=${DATA}/${PARTICLE}/${PARTICLE}_gen_${NOW}.slcio
 SIM=${DATA}/${PARTICLE}/${PARTICLE}_sim_${NOW}.slcio
+STEER=${TMP}/steer_reco_CONDOR.py
 
 export MUCOLL_GEO="${CODE}/detector-simulation/geometries/MuColl_10TeV_v0A/MuColl_10TeV_v0A.xml"
 
-echo "mkdir and cd"
+echo "mkdir"
 mkdir $TMP
-cd $TMP
 
 echo "cp and sed"
-cp ${CODE}/SteeringMacros/k4Reco/steer_reco_CONDOR.py ./ || return
-sed -i "s/INFILENAME/${NOW}/g" steer_reco_CONDOR.py || return
-sed -i "s/TYPEEVENT/${PARTICLE}/g" steer_reco_CONDOR.py || return
+cp ${CODE}/SteeringMacros/k4Reco/steer_reco_CONDOR.py ${STEER} || return
+sed -i "s/INFILENAME/${NOW}/g" ${STEER} || return
+sed -i "s/TYPEEVENT/${PARTICLE}/g" ${STEER} || return
 
 echo "pgun"
 time python ${CODE}/mucoll-benchmarks/generation/pgun/pgun_lcio.py -o -e ${EVENTS} -p 1 --pdg ${PDGID} --p ${ENERGY} --theta ${THETA} --phi ${PHI} -- ${GEN} || return
@@ -33,7 +36,7 @@ time python ${CODE}/mucoll-benchmarks/generation/pgun/pgun_lcio.py -o -e ${EVENT
 time ddsim --steeringFile ${CODE}/mucoll-benchmarks/simulation/ilcsoft/steer_baseline.py --inputFile ${GEN} --outputFile ${SIM} || return
 # time ddsim --inputFile ${GEN} --steeringFile /code/SteeringMacros/Sim/sim_steer_GEN_CONDOR.py --outputFile ${SIM}
 
-time k4run ./steer_reco_CONDOR.py # || return
+time k4run ${STEER} # || return
 # https://github.com/key4hep/k4FWCore/issues/125
 # time k4run ${CODE}/SteeringMacros/k4Reco/steer_reco_CONDOR.py || return
 # time Marlin --global.LCIOInputFiles=${SIM} --DD4hep.DD4hepXMLFile=${MUCOLL_GEO} ../mucoll-benchmarks/digitisation/marlin/digi_steer.xml
